41a5fc137b90f1ff6bd6b734fb39699bab0851fd
geco_rasp <geco.hh@gmail.com>
2019-02-04 15:29:30 +0000

    accepting all kinds of documents

--- bot_ficheros.py
+++ bot_ficheros.py
@@ -33,49 +33,78 @@ def help(bot, update):
 def echo(bot, update):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
-   
 
-def documento(bot, update):
-    pass
-    """
+def upload_named_file(bot, update, file_id, file_name):
+    newFile = bot.get_file(file_id)
+    newFile.download(file_name)
+    driveUploader.uploadFile(file_name)
+    update.message.reply_text(f'Uploaded {file_name}')
+    os.remove(file_name)
+
+def upload_unnamed_file(bot, update, file_id, chronological_filename):
+    file_name = ""
+    while not file_name or os.path.exists(file_name):
+        sleep(1)
+        file_name = chronological_filename()
+    upload_named_file(bot, update, file_id, file_name)
+
+
+def document(bot, update):
     file_id = update.message.document.file_id
     file_name = update.message.document.file_name
-    newFile = bot.get_file(file_id)
-    file_path = f'data/{file_name}'
-    newFile.download(file_path)
-    """
-    
-def foto(bot, update):
+    update.message.reply_text(f'document file received {file_name}')
+    upload_named_file(bot, update, file_id, file_name)
+
+
+def audio(bot, update):
+    file_id = update.message.audio.file_id
+    file_name = update.message.audio.file_name
+    update.message.reply_text(f'audio file received {file_name}')
+    upload_named_file(bot, update, file_id, file_name)
+
+
+def photo(bot, update):
     file_id = update.message.photo[-1]
-    file_name = photo_filename()
-    newFile = bot.get_file(file_id)
-    def download_photo(file_name):
-        file_path = f'{file_name}'
-        if os.path.exists(file_path):
-            sleep(1)
-            final_path = download_photo(photo_filename())
-        else: 
-            newFile.download(file_path)
-            final_path = file_path
-        return final_path
-    file_path = download_photo(file_name)
-    
-    driveUploader.uploadFile(file_path)
-    update.message.reply_text(f'Uploaded {file_name}')
-    os.remove(file_path)
+    upload_unnamed_file(bot, update, file_id, photo_filename)
+
+def video(bot, update):
+    file_id = update.message.video.file_id
+    update.message.reply_text(f'video file received {file_id}')
+    upload_unnamed_file(bot, update, file_id, video_filename)
+
+def video_note(bot, update):
+    file_id = update.message.video_note.file_id
+    update.message.reply_text(f'video_note received {file_id}')
+    upload_unnamed_file(bot, update, file_id, video_filename)
+
+def voice_note(bot, update):
+    file_id = update.message.voice.file_id
+    update.message.reply_text(f'voice_note received {file_id}')
+    upload_unnamed_file(bot, update, file_id, audio_filename)
+
 
 
 def photo_filename():
-    return f'photo_{now_substring()}.png'
+    return f'photo_{now_substring()}.jpg'
+
+def video_filename():
+    return f'video_{now_substring()}.mp4'
+
+def audio_filename():
+    return f'audio_{now_substring()}.ogg'
+
+
 
 def now_substring():
     #example string image_2019-01-14_16-01-20.png"
     format_string = "%Y-%m-%d_%H-%M-%S"
     return datetime.now().strftime(format_string)
 
+
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
+
 
 def main():
     """ credenciales de google drive """
@@ -94,12 +123,25 @@ def main():
     # on noncommand i.e message - echo the message on Telegram
     # dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_handler(MessageHandler(Filters.text, echo))
-
+    
     # get files
-    dp.add_handler(MessageHandler(Filters.document, documento))
+    dp.add_handler(MessageHandler(Filters.document, document))
+
+    # audio video
+    dp.add_handler(MessageHandler(Filters.video, video))
+
+    # audio audio
+    dp.add_handler(MessageHandler(Filters.audio, audio))
+
+    # video_note
+    dp.add_handler(MessageHandler(Filters.voice, voice_note))
+
+    # video_note
+    dp.add_handler(MessageHandler(Filters.video_note, video_note))
+
 
     # get photos
-    dp.add_handler(MessageHandler(Filters.photo, foto))
+    dp.add_handler(MessageHandler(Filters.photo, photo))
 
 
     # log all errors
